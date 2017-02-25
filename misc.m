ba8680b0d3cbb3d68fb3113ae355f5de88004b85
Roman <romanfirment@gmail.com>
2017-02-25 15:46:14 +0100

    horner bisect at zero

--- misc.m
+++ misc.m
@@ -67,28 +67,78 @@ endfunction
 % intvalinit('displaymidrad')
 % intvalinit('displayinfsup')
 % format long
-% opakujuci sa kod??
 
-function [hf, ok] = horner_without_zero(polynom_coefficients,x)
+function [hf, certainly_ok] = horner_left_zero(polynom_coefficients, x)
 
 	n = length(polynom_coefficients);
 
 	p(1) = intval(polynom_coefficients(1));
 
+	% getround(1);
+	xx = sup(x);
+
 	for i = 2:n
-			p(i) = p(i-1) * sup(x) + polynom_coefficients(i);
-		%p(i) = p(i-1) * (x^1) + polynom_coefficients(i);
+
+		if (inf(p(i-1)) < 0)
+			getround(-1);
+			left = p(i-1)*xx + polynom_coefficients(i);
+		else % save multiply by zero
+			left = polynom_coefficients(i);
+		endif
+
+		if (sup(p(i-1)) > 0)
+			getround(1);
+			right = p(i-1)*xx + polynom_coefficients(i);
+		else
+			right = polynom_coefficients(i);
+		endif
+
+		p(i) = infsup(inf(intval(left)), sup(intval(right)));
+
 	endfor
-    
+	
 	hf = p(n);
-	ok = "todo";
+	certainly_ok = false;
 
 endfunction
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%p = rand(1,1100)
-x = infsup(0.3,0.5);
+function op_polynomial = polynomial_opposite(polynom_coefficients)
+
+	n = length(polynom_coefficients);
+
+	sgn = -1;
+	op_polynomial(n) = polynom_coefficients(n);
+
+	for i = n-1:-1:1
+		op_polynomial(i) = sgn * polynom_coefficients(i); 
+		sgn *= (-1);
+	endfor
+
+endfunction
+
+function [hf, certainly_ok] = horner_bisect_at_zero(polynom_coefficients, x)
+
+	% todo check '0 in x'
+	left_interval  = infsup(inf(x),0);
+	right_interval = infsup(0,sup(x));
+
+	hf = hull(horner_left_zero(polynomial_opposite(polynom_coefficients),
+								-left_interval), 
+			  horner_left_zero(polynom_coefficients,right_interval));
+
+	certainly_ok = false
+
+endfunction
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%p = rand(1,1100);
+p = [1026,-470,53,-0.5];
+x = infsup(-0.5,0.15);
+
 tic
-[y,ok] = horner_simple(p, x)
-toc
+
+[y,ok] = horner_simple(p, x), toc
+%y = horner_left_zero(p, x),toc
+horner_bisect_at_zero(p,x), toc
+
 
