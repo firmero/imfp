a0616490df6a94431eba48b1f1a747bc5277a1dc
Roman <romanfirment@gmail.com>
2017-03-04 18:28:42 +0100

    Horner refactoring

--- misc.m
+++ misc.m
@@ -1,41 +1,84 @@
-% not a function file:
-% Prevent Octave from thinking that this
-% is a function file:
+% Not a function file:
+% Prevent Octave from thinking that this is a function file:
 1;
 
-% todo rounding
-% todo sparse polynoms, input form?
-% certainly_ok if true then HF not overestimate, if precise arithmetic
-function [hf, certainly_ok] = horner_simple(polynom_coefficients, x)
 
-	n = length(polynom_coefficients);
+% intvalinit('displaymidrad')
+% intvalinit('displayinfsup')
+% format long
+% allocate vector
+% p = repmat(intval(0),1,n);
 
-	p(1) = intval(polynom_coefficients(1));
+%%%-------------------------------------------------------------
 
-	% isnt it slow? append?
+%
+% The range of natural power over interval X (as a function) 
+%
+% To achieve:
+%
+%	[-8,1]=pow_3( [-2,1] )  !=  [-2,1]*[-2,1]*[-2,1]=[-8,4]
+%
+function res = interval_power(X,n)
+
+	if (even(n))
+		res = X^n;
+		return
+	endif
+
+	res = infsup(inf(X)^n, sup(X)^n);
+
+endfunction
+
+%% start of HORNER FORM
+
+%
+% Vector polynomial_coefficients [a_1, a_2, ..., a_n] is interpreted as polynom:
+%
+%	p(x) = a_1*x^(n-1) + a_2*x^(n-2) + ... + a_(n-1)*x^1 + a_n
+%
+% Return:
+%
+%	hf				- Horner form
+%	certainly_ok	- if true then Horner form gives no overestimation 
+%
+function [hf, certainly_ok] = horner_form(polynomial_coefficients, X)
+
+	%{ 
+	% gives better result but is expensive
+	if (inf(X) < 0)
+		hf = horner_form_bisect_zero(polynomial_coefficients,X);
+		certainly_ok = false;
+		return
+	endif
+	%}
+
+	n = length(polynomial_coefficients);
+	% allocate vector
+	p = repmat(intval(0),1,n);
+
+	p(1) = intval(polynomial_coefficients(1));
+
 	for i = 2:n
-		p(i) = p(i-1) * x + polynom_coefficients(i);
-		%p(i) = p(i-1) * (x^1) + polynom_coefficients(i);
+		p(i) = p(i-1) * X + polynomial_coefficients(i);
 	endfor
     
 	hf = p(n);
 
-	if (inf(x) == sup(x))
+	% tests for covering overestimation interval
+	if (inf(X) == sup(X))
 		certainly_ok = true; % what if coefficients are intervals
 		return
 	endif
 
-	if (in(0,intval(x)))
+	if (in(0,intval(X)))
 		certainly_ok = false; % what if coefficients are intervals
 		return
 	endif
 
-	sgn = sign(polynom_coefficients(1));
-	% not working on interval koefficients
+	% !! not working on interval coefficients !!
+	sgn = sign(polynomial_coefficients(1));
 
-
-	% !! duplicity of code
-	if (inf(x) >= 0 )		% on right
+	if (inf(X) >= 0 )		% on right
 		for i = 2:n-1
 			if (inf(sgn*p(i)) < 0)
 				certainly_ok = false;
@@ -56,44 +99,34 @@ function [hf, certainly_ok] = horner_simple(polynom_coefficients, x)
 
 endfunction
 
-function pplot(polynom, interval)
+%
+% Horner form for X = [0,R]
+%
+function hf = horner_form_left_zero(polynomial_coefficients, X)
 
-	x = linspace(inf(interval), sup(interval));
-	y = polyval(polynom,x);
-	min(y)
-	max(y)
-	plot(x, y);
+	n = length(polynomial_coefficients);
+	% allocate vector
+	p = repmat(intval(0),1,n);
 
-endfunction
-
-% intvalinit('displaymidrad')
-% intvalinit('displayinfsup')
-% format long
-
-% if x is [0,R]
-function [hf, certainly_ok] = horner_left_zero(polynom_coefficients, x)
-
-	n = length(polynom_coefficients);
-
-	p(1) = intval(polynom_coefficients(1));
+	p(1) = intval(polynomial_coefficients(1));
 
 	% getround(1);
-	xx = sup(x);
+	xx = sup(X);
 
 	for i = 2:n
 
 		if (inf(p(i-1)) < 0)
 			getround(-1);
-			left = p(i-1)*xx + polynom_coefficients(i);
+			left = p(i-1)*xx + polynomial_coefficients(i);
 		else % save multiply by zero
-			left = polynom_coefficients(i);
+			left = polynomial_coefficients(i);
 		endif
 
 		if (sup(p(i-1)) > 0)
 			getround(1);
-			right = p(i-1)*xx + polynom_coefficients(i);
+			right = p(i-1)*xx + polynomial_coefficients(i);
 		else
-			right = polynom_coefficients(i);
+			right = polynomial_coefficients(i);
 		endif
 
 		p(i) = infsup(inf(intval(left)), sup(intval(right)));
@@ -101,98 +134,116 @@ function [hf, certainly_ok] = horner_left_zero(polynom_coefficients, x)
 	endfor
 	
 	hf = p(n);
-	certainly_ok = false;
 
 endfunction
 
-function op_polynomial = polynomial_opposite(polynom_coefficients)
+%
+% Invert polynomial coefficients
+%
+function op_polynomial = invert_polynomial(polynomial_coefficients)
 
-	n = length(polynom_coefficients);
+	n = length(polynomial_coefficients);
 
 	sgn = -1;
-	op_polynomial(n) = polynom_coefficients(n);
+	op_polynomial(n) = polynomial_coefficients(n);
 
 	for i = n-1:-1:1
-		op_polynomial(i) = sgn * polynom_coefficients(i); 
+		op_polynomial(i) = sgn * polynomial_coefficients(i); 
 		sgn *= (-1);
 	endfor
 
 endfunction
 
-function [hf, certainly_ok] = horner_bisect_at_zero(polynom_coefficients, x)
+%
+% Horner form for X containing 0
+%
+function hf = horner_form_bisect_zero(polynomial_coefficients, X)
 
-	% todo check '0 in x'
-	left_interval  = infsup(inf(x),0);
-	right_interval = infsup(0,sup(x));
+	if (!in(0,X))
+		error("Interval doesn't contain 0")
+		return
+	endif
 
-	hf = hull(horner_left_zero(polynomial_opposite(polynom_coefficients),
+	left_interval  = infsup(inf(X),0);
+	right_interval = infsup(0,sup(X));
+
+	hf = hull(horner_form_left_zero(invert_polynomial(polynomial_coefficients),
 								-left_interval), 
-			  horner_left_zero(polynom_coefficients,right_interval));
-
-	certainly_ok = false;
+			  horner_form_left_zero(polynomial_coefficients,right_interval));
 
 endfunction
 
-function f_derivated = derivate_polynom(polynom_coefficients)
+%% end of HORNER
 
-	n = length(polynom_coefficients);
 
-	% co je rychlejsie, downto or for, append
+%% start of MEAN VAL FORM
+
+%
+% Compute derivative of p(x)
+%
+%	p(x) = a_1*x^(n-1) + a_2*x^(n-2) + ... + a_(n-1)*x^1 + a_n
+%
+function p_derivated = derivate_polynomial(polynomial_coefficients)
+
+	n = length(polynomial_coefficients);
+	p_derivated = repmat(intval(0),1,n-1);
+
 	for i = 1:n-1
-
-		% rounding?
-		f_derivated(i) = i * polynom_coefficients(i+1);
-
+		p_derivated(i) = (n-i) * polynomial_coefficients(i);
 	endfor
 
 endfunction
 
-% poradie koeficienov?
-function mvf = mean_value_form(polynom_coefficients, x)
+%
+% Vector polynomial_coefficients [a_1, a_2, ..., a_n] is interpreted as polynom:
+%
+%	p(x) = a_1*x^(n-1) + a_2*x^(n-2) + ... + a_(n-1)*x^1 + a_n
+%
+function mvf = mean_value_form(polynomial_coefficients, x)
 
-	polynom_coefficients = fliplr(polynom_coefficients);
 	c = mid(x);
+	hf_at_center = horner_form(polynomial_coefficients,c);
 
-	hf_at_center = horner_simple(polynom_coefficients,c);
+	p_derivated = derivate_polynomial(polynomial_coefficients,x);
 
-	n = length(polynom_coefficients);
-
-
-	f_derivated = derivate_polynom(polynom_coefficients,x);
-
-
-	hf_derivated = horner_simple(fliplr(f_derivated), x);
+	hf_derivated = horner_form(p_derivated,x);
 
 	mvf = hf_at_center + hf_derivated*(x-c);
 
 endfunction
 
-% poradie?
-function sf = slope_form(polynom_coefficients, x)
+%
+% Vector polynomial_coefficients [a_1, a_2, ..., a_n] is interpreted as polynom:
+%
+%	p(x) = a_1*x^(n-1) + a_2*x^(n-2) + ... + a_(n-1)*x^1 + a_n
+%
+function sf = slope_form(polynomial_coefficients, x)
 
-	polynom_coefficients = fliplr(polynom_coefficients);
-	n = length(polynom_coefficients);
+	n = length(polynomial_coefficients);
 	c = mid(x);
 
+
 	% get coefficients of polynom g()
-	g(n-1) = polynom_coefficients(n);
+	g = repmat(intval(0),1,n-1);
+
+	g(n-1) = polynomial_coefficients(n);
 	for i = n-1:-1:2
-		g(i-1) = g(i)*c + polynom_coefficients(i);
+		g(i-1) = g(i)*c + polynomial_coefficients(i);
 	endfor
 
 	%for testing purpose
 	%{ 
-	polynom_coefficients = fliplr(polynom_coefficients)
+	polynomial_coefficients = fliplr(polynomial_coefficients)
 	g = fliplr(g)
-	xx=2.345; ((polyval(polynom_coefficients,c) + polyval(g,xx)*(xx-c)) == \
-				polyval(polynom_coefficients,xx))
+	xx=2.345; ((polyval(polynomial_coefficients,c) + polyval(g,xx)*(xx-c)) == \
+				polyval(polynomial_coefficients,xx))
 	%} 
 
 
-	hf_g = horner_simple(g,x);
+	hf_g = horner_form(g,x);
 
 	% what if interval coefficients?
-	f_at_c = polyval(polynom_coefficients, c);
+	f_at_c = polyval(polynomial_coefficients, c);
 
 	sf = f_at_c + hf_g*(x-c);
 
@@ -222,47 +273,48 @@ function [c_left, c_right] = centres_mean_value_form(f_derivated, x)
 endfunction
 
 % poradie?
-function mvfb = mean_value_form_bicentred(polynom_coefficients,x)
+function mvfb = mean_value_form_bicentred(polynomial_coefficients,x)
 
-	polynom_coefficients = fliplr(polynom_coefficients);
-	f_derivated = fliplr(derivate_polynom(polynom_coefficients));
+	polynomial_coefficients = fliplr(polynomial_coefficients);
+	f_derivated = fliplr(derivate_polynomial(polynomial_coefficients));
 
-	hf_derivated = horner_simple(f_derivated,x);
+	hf_derivated = horner_form(f_derivated,x);
 
 	[c_left, c_right] = centres_mean_value_form(hf_derivated,x);
 
 	% to do rounding
-	right = horner_simple(polynom_coefficients,c_right) + sup(hf_derivated*(x-c_right));
-	left = horner_simple(polynom_coefficients,c_left) + inf(hf_derivated*(x-c_left));
+	right = horner_form(polynomial_coefficients,c_right) + sup(hf_derivated*(x-c_right));
+	left = horner_form(polynomial_coefficients,c_left) + inf(hf_derivated*(x-c_left));
 
 	mvfb = infsup(inf(left),sup(right));
 
 endfunction
 
+%% end of MEAN VAL FORM
 %
 %  tc(1) = hf(p,c), tc(2) = hf(p',c),...
 %
-function tc = taylor_coefficient(polynom_coefficients, c)
+function tc = taylor_coefficient(polynomial_coefficients, c)
 
-	n = length(polynom_coefficients);
+	n = length(polynomial_coefficients);
 
 	%tc = zeros(1,n);
 
-	tc(1) = horner_simple(polynom_coefficients,c);
+	tc(1) = horner_form(polynomial_coefficients,c);
 
 	fact = 1;
 
 	for j = 2:n
 		k = 2;
-		polynom_coefficients(n) = polynom_coefficients(n-1);
+		polynomial_coefficients(n) = polynomial_coefficients(n-1);
 		for i = n-1:-1:j
-			polynom_coefficients(i) = polynom_coefficients(i-1)*k;
+			polynomial_coefficients(i) = polynomial_coefficients(i-1)*k;
 			k++;
 		endfor
 		
-		p = polynom_coefficients(j:n);
+		p = polynomial_coefficients(j:n);
 
-		tc(j) = horner_simple(p,c) / fact;
+		tc(j) = horner_form(p,c) / fact;
 		% todo overflow
 		fact *= j;
 
@@ -270,16 +322,16 @@ function tc = taylor_coefficient(polynom_coefficients, c)
 
 endfunction
 
-function tf = taylor_form(polynom_coefficients, x)
+function tf = taylor_form(polynomial_coefficients, x)
 
 	%todo check special cases for x
 
 	c = mid(x);
 	r = rad(x);
 
-	n = length(polynom_coefficients);
+	n = length(polynomial_coefficients);
 
-	tay_coeff = taylor_coefficient(polynom_coefficients,c);
+	tay_coeff = taylor_coefficient(polynomial_coefficients,c);
 
 	% magnitude....
 	% rounding...
@@ -296,7 +348,7 @@ function tf = taylor_form(polynom_coefficients, x)
 
 endfunction
 
-function y = evaluate_parallel(polynom_coefficients, x)
+function y = evaluate_parallel(polynomial_coefficients, x)
 
 	ncpus = nproc();
 
@@ -316,7 +368,7 @@ function y = evaluate_parallel(polynom_coefficients, x)
 		intervals(i) = infsup(left,right);
 		left = right;
 
-		coefficients(i) = polynom_coefficients;
+		coefficients(i) = polynomial_coefficients;
 
 	endfor
 
@@ -333,18 +385,18 @@ endfunction
 %
 % x interval
 %
-function y = evaluate(polynom_coefficients, x)
+function y = evaluate(polynomial_coefficients, x)
 
 	t = inf(x);
-	y = intval(polyval(polynom_coefficients,t));
+	y = intval(polyval(polynomial_coefficients,t));
 
 	while (t < sup(x))
 		t += 0.0003;
-		ny = polyval(polynom_coefficients,t);
+		ny = polyval(polynomial_coefficients,t);
 		y = hull(y,ny);
 	endwhile
 
-	y = hull(y,polyval(polynom_coefficients,sup(x)));
+	y = hull(y,polyval(polynomial_coefficients,sup(x)));
 
 endfunction
 
@@ -371,7 +423,7 @@ endfunction
 function parabola_range = evaluate_parabola(a2,a1,a0,x)
 	
 	if (in0(0,x))
-		parabola_range = horner_simple([a2 a1 a0],x);
+		parabola_range = horner_form([a2 a1 a0],x);
 		return
 	endif
 
@@ -393,16 +445,16 @@ endfunction
 %
 % [ a_1 a_2 ... a_(n+1) ]
 %
-function res = interpolation_form(polynom_coefficients,x)
+function res = interpolation_form(polynomial_coefficients,x)
 	
-	f_derivated = fliplr(derivate_polynom(fliplr(polynom_coefficients)));
-	f_twice_derivated = fliplr(derivate_polynom(fliplr(f_derivated)));
+	f_derivated = fliplr(derivate_polynomial(fliplr(polynomial_coefficients)));
+	f_twice_derivated = fliplr(derivate_polynomial(fliplr(f_derivated)));
 
 	c = mid(x);
 
-	f_at_c = horner_simple(polynom_coefficients,c);
-	f_derivated_at_c = horner_simple(f_derivated,c);
-	f_twice_derivated_range = horner_simple(f_twice_derivated,x);
+	f_at_c = horner_form(polynomial_coefficients,c);
+	f_derivated_at_c = horner_form(f_derivated,c);
+	f_twice_derivated_range = horner_form(f_twice_derivated,x);
 
 	m = mid(f_twice_derivated_range);
 
@@ -419,15 +471,15 @@ function res = interpolation_form(polynom_coefficients,x)
 
 endfunction
 
-function res = modified_interpolation_form(polynom_coefficients,x) 
-	f_derivated = fliplr(derivate_polynom(fliplr(polynom_coefficients)));
-	f_twice_derivated = fliplr(derivate_polynom(fliplr(f_derivated)));
+function res = modified_interpolation_form(polynomial_coefficients,x) 
+	f_derivated = fliplr(derivate_polynomial(fliplr(polynomial_coefficients)));
+	f_twice_derivated = fliplr(derivate_polynomial(fliplr(f_derivated)));
 
 	c = mid(x);
 
-	f_at_c = horner_simple(polynom_coefficients,c);
-	f_derivated_at_c = horner_simple(f_derivated,c);
-	f_twice_derivated_range = horner_simple(f_twice_derivated,x);
+	f_at_c = horner_form(polynomial_coefficients,c);
+	f_derivated_at_c = horner_form(f_derivated,c);
+	f_twice_derivated_range = horner_form(f_twice_derivated,x);
 
 	m = mid(f_twice_derivated_range);
 
@@ -462,7 +514,7 @@ function test1
 		x = infsup(-0.2,0.3);
 
 		evaluate_parallel(p,x);
-		disp "  HORNER ",horner_simple(p,x),sup(ans)-inf(ans), toc, 
+		disp "  HORNER ",horner_form(p,x),sup(ans)-inf(ans), toc, 
 		disp "  MEAV_VAL ",mean_value_form(p,x),sup(ans)-inf(ans), toc
 		disp "  MEAN_SLOPE ",slope_form(p,x),sup(ans)-inf(ans), toc
 		%disp "  MEAN_BICENTRED ", mean_value_form_bicentred(p,x),sup(ans)-inf(ans), toc
@@ -483,14 +535,14 @@ p = [4,3,6];				% [5.7400, 6.7601]
 x = infsup(-0.1,0.2);
 tic
 
-%[y,ok] = horner_simple(p, x), toc
+%[y,ok] = horner_form(p, x), toc
 %y = horner_left_zero(p, x),toc
-%horner_bisect_at_zero(p,x), toc
+%horner_form_bisect_zero(p,x), toc
 
 %p = [infsup(2,2.3), infsup(4,4.2)];
-%horner_simple(p,x), toc
+%horner_form(p,x), toc
 
-%disp "  HORNER ",horner_simple(p,x), toc
+%disp "  HORNER ",horner_form(p,x), toc
 %disp "  MEAV_VAL ",mean_value_form(p,x), toc
 %disp "  MEAN_SLOPE ",slope_form(p,x), toc
 %disp "  MEAN_BICENTRED ", mean_value_form_bicentred(p,x), toc
@@ -510,13 +562,13 @@ tic
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function res = bernstein_coefficients(polynom_coefficients,x)
+function res = bernstein_coefficients(polynomial_coefficients,x)
 
 	w = sup(x) - inf(x);
-	n = length(polynom_coefficients);
+	n = length(polynomial_coefficients);
 
 	% k should be at least n-1 (deg of polynom)
-	k = n-1 % todo
+	k = n-1; % todo
 
 	% temporary, not bernstein coefficients
 	b(1) = intval(1);
@@ -529,12 +581,10 @@ function res = bernstein_coefficients(polynom_coefficients,x)
 		q += w; % trick to simulate factorial
 	endfor
 	
-
-	tc = taylor_coefficient(polynom_coefficients,inf(x))
+	tc = taylor_coefficient(polynomial_coefficients,inf(x));
 	for i = 1:n
 		b(i) = b(i)*tc(i);
 	endfor
-	b
 
 	res = b(1)
 	for j = 1:k
@@ -545,16 +595,25 @@ function res = bernstein_coefficients(polynom_coefficients,x)
 
 		% b(1) is bernstein coeffcient b1,b2,b3,bj.. after a loop of j
 		res = hull(res,b(1));
-		b(1)
 	endfor
-	
+
 
 endfunction
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-x=infsup(0,1);
-p = intval("-1 0.7 -0.2 0.9");
+x=infsup(-0.3,0.2);
+
+p = rand(1,7);
+p = p - 0.5;
+
+tic, evaluate_parallel(p,x), toc
+tic, horner_form(p,x), toc
+tic, mean_value_form(p,x), toc
+tic, slope_form(p,x), toc
 
 %tic, evaluate_parallel(p,x), toc
 
-bernstein_coefficients(p,x)
+
+%bernstein_coefficients(p,x)
+%horner_form(p,x)
+
