cc5574a7c138aa01b7b65a720d8cf049884fc659
Roman Firment <romanfirment@matfyz.cz>
2017-03-24 14:29:40 +0100

    refactor

--- imfp.m
+++ imfp.m
@@ -4,27 +4,25 @@
 function imfp(par_opt)
 
 	has_intval_arithmetic = which('infsup');
-
 	if (isempty(has_intval_arithmetic))
 		disp('---- Please initialize INTLAB, then call this function again.');
 		return
 	end
 
-	global IFMP_DIR;
-
 	loc = which('imfp.m');
-	IFMP_DIR = loc(1:end-6);
+	IMFP_DIR = loc(1:end-6);
 
-	addpath( [ IFMP_DIR filesep 'forms' ] );
-	addpath( [ IFMP_DIR filesep 'misc' ] );
-	addpath( [ IFMP_DIR filesep 'tests' ] );
+	addpath( [ IMFP_DIR filesep 'forms' ] );
+	addpath( [ IMFP_DIR filesep 'misc' ] );
+	addpath( [ IMFP_DIR filesep 'tests' ] );
 
 	running_octave = 0 ~= exist('OCTAVE_VERSION', 'builtin'); 
 
 	if (running_octave)
-		addpath( [ IFMP_DIR filesep 'octave_env' ] );
+		% to support octave specific functions
+		addpath( [ IMFP_DIR filesep 'octave_env' ] );
 	%else
-	%	addpath( [ IFMP_DIR filesep 'matlab_env' ] );
+	%	addpath( [ IMFP_DIR filesep 'matlab_env' ] );
 	end
 
 	% 1 for parallel
@@ -42,11 +40,11 @@ function imfp(par_opt)
 			warning('Pkg parallel not found');
 
 			disp 'Installing pkg struct-1.0.14.tar.gz'
-			pkg('install',[ IFMP_DIR filesep 'lib' filesep 'struct-1.0.14.tar.gz']);
+			pkg('install',[ IMFP_DIR filesep 'lib' filesep 'struct-1.0.14.tar.gz']);
 			disp 'Pkg struct-1.0.14.tar.gz installed'
 
 			disp 'Installing pkg parallel-3.1.1.tar.gz'
-			pkg('install',[ IFMP_DIR filesep 'lib' filesep 'parallel-3.1.1.tar.gz']);
+			pkg('install',[ IMFP_DIR filesep 'lib' filesep 'parallel-3.1.1.tar.gz']);
 			disp 'Pkg parallel-3.1.1.tar.gz installed'
 
 		end
@@ -57,23 +55,23 @@ function imfp(par_opt)
 		% running script makes local functions global in octave,
 		% not working in matlab :/
 		load_interval_forms_par
-		addpath( [ IFMP_DIR filesep 'misc/evaluate_polynomial/private' ] );
-		disp 'paralell'
+		addpath( [ IMFP_DIR filesep 'misc/evaluate_polynomial/private' ] );
+		disp 'paralell...'
 
 		main
 		return
 	end
 
 	if (par && ~running_octave)
-		warning('Parallelization cannot be established');
+		warning('Parallelization cannot be established.');
 	end
 
-	disp 'no paralell'
+	disp 'no paralell...'
 	% in matlab cannot promote local function to global
 	% by calling script :/
-	addpath( [ IFMP_DIR filesep 'misc/interval_polynomial_forms' ] );
+	addpath( [ IMFP_DIR filesep 'misc/interval_polynomial_forms' ] );
 	% shade parallel version
-	addpath( [ IFMP_DIR filesep 'misc/evaluate_polynomial' ] );
+	addpath( [ IMFP_DIR filesep 'misc/evaluate_polynomial' ] );
 
 	main
 	return
@@ -83,5 +81,4 @@ end
 function main
 
 	run_tests
-
 end
