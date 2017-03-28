6e6f05ecf31e5dd5fb121b2cbde5c2e8cebfbcec
Roman <romanfirment@gmail.com>
2017-03-29 00:52:04 +0200

    comments

--- forms/pvbernsteinbzenc.m
+++ forms/pvbernsteinbzenc.m
@@ -1,4 +1,4 @@
-function res = pvbernsteinbzenc(polynomial_coefficients,X,k)
+function [ibfbz ver] = pvbernsteinbzenc(p,ix,k)
 %BEGINDOC==================================================================
 % .Author
 %
@@ -7,16 +7,29 @@ function res = pvbernsteinbzenc(polynomial_coefficients,X,k)
 %--------------------------------------------------------------------------
 % .Description.
 %
-%  Bernstein form for X containing 0.
+%  Bernstein form with bisection at the zero.
 %
 %--------------------------------------------------------------------------
 % .Input parameters.
 %
+%  ix ... interval x
+%  p  ... vector of polynomial coefficients [a_1 ... a_n]
+%  k  ... optional, should be at least deg(p), which is default value.
+%         greater value leads to tighter enclosure
+%
+%	p(x) = a_1*x^(n-1) + a_2*x^(n-2) + ... + a_(n-1)*x^1 + a_n
+%
 %--------------------------------------------------------------------------
 % .Output parameters.
 %
+%  ibfbz ... Bernstein form with bisection at the zero.
+%  ver   ... 1 iff Bernstein form is exact, otherwise 0
+%
 %--------------------------------------------------------------------------
 % .Implementation details.
+%
+%  For ix containing the zero evaluate Bernstein form of p over [0,sup(ix)]
+%  and Bernstein form of p(-x) over [0,-inf(ix)].
 %
 %--------------------------------------------------------------------------
 % .License.
@@ -34,24 +47,24 @@ function res = pvbernsteinbzenc(polynomial_coefficients,X,k)
 %
 %ENDDOC====================================================================
 
-n = length(polynomial_coefficients);
-%todo
-k = -321;
+n = length(p);
 
-if (~in(0,X))
-	warning('Interval X doesn''t contain 0');
-	res = pvbernsteinenc(polynomial_coefficients,X,k);
-	return
-end
-
-if (k == -321)
+% k should be at least n-1 (deg of polynomial)
+if (nargin() == 2)
 	k = n-1;
-elseif (k< n-1)
+% not hadle bad calling
+elseif (k < n-1)
 	warning('Parameter k should be at least the degree of polynomial');
 	k = n-1;
 end
 
-right = pvbernsteinenc(polynomial_coefficients,infsup(0,sup(X)), k);
+if (~in(0,ix))
+	warning('Interval ix doesn''t contain 0');
+	ibfbz = pvbernsteinenc(p,ix,k);
+	return
+end
+
+[iright verright] = pvbernsteinenc(p,infsup(0,sup(ix)), k);
 
 % coefficients for p(-x)
 start = 1;
@@ -60,11 +73,19 @@ if (odd(n))
 end
 
 for i = start:2:n
-	polynomial_coefficients(i) = polynomial_coefficients(i) * (-1);
+	p(i) = p(i) * (-1);
 end
 
-left = pvbernsteinenc(polynomial_coefficients,infsup(0,-inf(X)), k);
 
-res = hull(left, right);
+[ileft verleft] = pvbernsteinenc(p,infsup(0,-inf(ix)), k);
+
+ibfbz = hull(ileft, iright);
+
+% overestimation
+if (verleft + verright == 2)
+	ver = 1;
+else 
+	ver = 0;
+end
 
 end
--- forms/pvbernsteinenc.m
+++ forms/pvbernsteinenc.m
@@ -1,4 +1,4 @@
-function ibf = pvbernsteinenc(p,ix,k)
+function [ibf ver] = pvbernsteinenc(p,ix,k)
 %BEGINDOC==================================================================
 % .Author
 %
@@ -24,6 +24,7 @@ function ibf = pvbernsteinenc(p,ix,k)
 % .Output parameters.
 %
 %  ibf ... Bernstein form
+%  ver ... 1 iff Bernstein form is exact, otherwise 0
 %
 %--------------------------------------------------------------------------
 % .Implementation details.
@@ -56,24 +57,23 @@ function ibf = pvbernsteinenc(p,ix,k)
 %  scheme:
 %
 %  d = degree of p
-%
-%  for i=1..d+1
+%  n = deg + 1
+%  
+%  for i=1..n 
 %   b_i_0 = width(ix)^(i-1) / (k over (i-1)) * tay_coeff((i-1),inf(ix))
 %         =(width(ix)^(i-1) / (k*(k-1)*...*(k-i+2))) * (i-1)! *
 %           * tay_coeff((i-1),inf(ix))
 %
-%  for j = 1..k-d                      b_(d+1)_j = b_(d+1)_0
+%  for j = 1..k-d                        v_n_j = v_n_0
 %
-%  for j = 1..k   i=0..min(d-1, k-j)   b_i_j = b_i_(j-1) + b_(i+1)_(j-1)   
+%  for j = 1..k   i=1..min(n-1, k-j+1)   v_i_j = v_i_(j-1) + v_(i+1)_(j-1) 
 %
-%  Then b_j = b_1_j     for j=0..k
+%  Then b_j = v_1_j     for j=0..k
 %
-%  The implementation use only one dimensional array b.
-%  Direction j symboles a j-th computation round. todo
+%  The implementation use only one dimensional array iv size of n.
+%  Direction j symbolizes the j-th computation round. After the j-th loop
+%  iv(i) = iv_i_j for all i where iv_i_j is defined.
 %
-
-
-
 %--------------------------------------------------------------------------
 % .License.
 %
@@ -95,7 +95,8 @@ n = length(p);
 % k should be at least n-1 (deg of polynomial)
 if (nargin() == 2)
 	k = n-1;
-elseif (k< n-1)
+% not hadle bad calling
+elseif (k < n-1)
 	warning('Parameter k should be at least the degree of polynomial');
 	k = n-1;
 end
@@ -103,39 +104,56 @@ end
 w = sup(ix) - inf(ix);
 
 % used for computation of bernstein coefficients
-ib = repmat(intval(0),1,n);
+iv = repmat(intval(0),1,n);
 
 % temporary, this doesn't represent all bernstein coefficients
-ib(1) = intval(1);
-% ib(1) = ib_1_1
+iv(1) = intval(1);
 
-% to simulate factorial
+% to simulate factorial (i-1)! and w^(i-1)
 q = w;
+
 for i = 2:n
-	ib(i) = ib(i-1)*q/(k-i+2);
+	iv(i) = iv(i-1)*q/(k-i+2);
 	q = q + w; % trick to simulate factorial
 end
-% ib(1) = 1
-% ib(i) = w^(i-1)*(i-1)! / (k*(k-1)*...*(k-i+2))   for i=2..n
+% iv(1) = 1
+% iv(i) = w^(i-1)*(i-1)! / (k*(k-1)*...*(k-i+2))   for i=2..n
 
 
-tc = taylor_coefficients_(p,inf(ix));
-% tc(i) = HF( (i-1) derivative of p, inf(ix))/ (i-1)!
+itc = taylor_coefficients_(p,inf(ix));
+% itc(i) = HF( (i-1) derivative of p, inf(ix))/ (i-1)!
 for i = 1:n
-	ib(i) = ib(i)*tc(i);
+	iv(i) = iv(i)*itc(i);
 end
-% first round finished:
-%   ib(i) = b_i_1   for i=1..d+1
+% zero round (j=0) finished:
+%   iv(i) = v_i_0   for i=1..n
 
-% scheme to compute iteratively bernstein coefficients (stored in ib(1))
-ibf = ib(1);
+
+% scheme to compute iteratively bernstein coefficients
+% after the j-th loop iv(1) is a b_j 
+
+% v(n) is never modified, so after the j-th round 
+% it holds that v_n_j = v_n_0 = v(n)
+
+% now iv(1) = iv_1_0 = b_0
+ibf = iv(1);
+ib_0 = iv(1);
 for j = 1:k
 	for i = 1:min(n-1,k-j+1)
-		ib(i) = ib(i) + ib(i+1);
+		iv(i) = iv(i) + iv(i+1);
 	end
 
-	% ib(1) is bernstein coeffcient <- b1,b2,b3,bj.. after the j-th loop
-	ibf = hull(ibf,ib(1));
+	% iv(1) = b_j
+	ibf = hull(ibf,iv(1));
+end
+ib_k = iv(1);
+
+% overestimation test
+itmp = hull(ib_0, ib_k);
+if (itmp == ibf)
+	ver = 1;
+else 
+	ver = 0;
 end
 
 end
