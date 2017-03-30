372626b4a04c210473db9950977388ed97103b58
Roman <romanfirment@gmail.com>
2017-03-30 21:32:07 +0200

    commented

--- forms/pvinterpolationenc.m
+++ forms/pvinterpolationenc.m
@@ -1,4 +1,4 @@
-function res = pvinterpolationenc(polynomial_coefficients,X)
+function iif = pvinterpolationenc(p,ix)
 %BEGINDOC==================================================================
 % .Author
 %
@@ -7,24 +7,29 @@ function res = pvinterpolationenc(polynomial_coefficients,X)
 %--------------------------------------------------------------------------
 % .Description.
 %
+%  Interpolation form uses Taylor expansion of p at the middle c of ix:
+%  p(x) = p(c) + p`(c)*(x-c) + 0/5*p``(y)*(x-c)^2 for some y between
+%  x and c.
+%  Then for any m:
+%  p(x) = p(c) + p`(c)*(x-c) + 0.5*m*(x-c)^2 + 0.5*(p``(y)-m)*(x-c)^2
 %
-% Vector polynomial_coefficients [a_1, a_2, ..., a_n] is interpreted as polynom:
+%  Interpolation form uses mid(HF(p``,ix)) as m.
+%  IF(ix) = HF(parabola,ix) + 0.5*(HF(p``,ix) - m)*(ix-c)^2
 %
-%	p(x) = a_1*x^(n-1) + a_2*x^(n-2) + ... + a_(n-1)*x^1 + a_n
+%  parabola(x) = 0.5*m*x^2 + (p`(c) - m*c)*x + (p(c) - p`(c)*c + 0.5*m*c^2)
 %
-% m = mid(HF(p``,X))
-% c = mid(X)
+%  Where HF is Horner form.
 %
-% IF(X) = HF(parabola,X) + 0.5*(HF(p``,X) - m)*(X-c)^2
-%
-% parabola(x) = 0.5*m*x^2 + (p`(c) - m*c)*x + (p(c) - p`(c)*c + 0.5*m*c^2)
-%
-	
 %--------------------------------------------------------------------------
 % .Input parameters.
 %
+%  p  ... vector of polynomial coefficients [a_1 ... a_n]
+%  ix ... interval x
+%
 %--------------------------------------------------------------------------
 % .Output parameters.
+%
+%  iif ... Interpolation form
 %
 %--------------------------------------------------------------------------
 % .Implementation details.
@@ -45,26 +50,37 @@ function res = pvinterpolationenc(polynomial_coefficients,X)
 %
 %ENDDOC====================================================================
 
-p_derivated = derivate_polynomial(polynomial_coefficients);
-p_twice_derivated = derivate_polynomial(p_derivated);
+% interval coefficients of derivative of p
+ip_der = derivate_polynomial(p);
 
-c = mid(X);
+% interval coefficients of second derivative of p
+ip_der2 = derivate_polynomial(ip_der);
 
-p_at_c = pvhornerenc(polynomial_coefficients,intval(c));
-p_derivated_at_c = pvhornerenc(p_derivated,intval(c));
-p_twice_derivated_range = pvhornerenc(p_twice_derivated,X);
+c = mid(ix);
 
-m = mid(p_twice_derivated_range);
+% to find out parabola coeffcients the following values are needed
+% p(c), p`(c) and p``(ix)
+ip_at_c = pvhornerenc(p,c);
+ip_der_at_c = pvhornerenc(ip_der,c);
+ip_der2_val = pvhornerenc(ip_der2,ix);
+%  Interpolation form uses mid(HF(p``,ix)) as m.
+im = intval(mid(ip_der2_val));
 
 % parabola coefficients
-a2 = 0.5*m;
-a1 = p_derivated_at_c - m*c;
-a0 = (a2*c - p_derivated_at_c)*c + p_at_c;
+% parabola(x) = 0.5*m*x^2 + (p`(c) - m*c)*x + (p(c) - p`(c)*c + 0.5*m*c^2)
+ia2 = 0.5*im;
+ia1 = ip_der_at_c - im*c;
+ia0 = (ia2*c - ip_der_at_c)*c + ip_at_c;
 
-parabola_range = evaluate_parabola(a2,a1,a0,X);
+iparabola_val = evaluate_parabola(ia2,ia1,ia0,ix);
+
+oldmod = getround();
 
 setround(1);
-r = mag(X-c);
-res = parabola_range + (p_twice_derivated_range - m)*infsup(0,0.5*r*r);
+r = mag(ix-c);
 
+% IF(ix) = HF(parabola,ix) + 0.5*(HF(p``,ix) - m)*(ix-c)^2
+iif = iparabola_val + (ip_der2_val - im)*infsup(0,0.5*r*r);
+
+setround(oldmod);
 end
