8c81e88c6d3fb211cd7a4ebd08c4f8193a5da761
Roman <romanfirment@gmail.com>
2017-04-01 23:59:59 +0200

    commented

--- forms/private/taylor_form_eval_half_.m
+++ forms/private/taylor_form_eval_half_.m
@@ -1,4 +1,4 @@
-function res = taylor_form_eval_half_(tay_coeff,r)
+function itfh = taylor_form_eval_half_(itay_coeff,r)
 %BEGINDOC==================================================================
 % .Author
 %
@@ -7,18 +7,26 @@ function res = taylor_form_eval_half_(tay_coeff,r)
 %--------------------------------------------------------------------------
 % .Description.
 %
+%  This function is used by pvtaylorbm(), which converts his input interval
+%  to centered interval [-r,r]. Then it calls that function
+%  twice and returns hull of results returned by that function.
 %
-% This function is used by pvtaylorbm(), which converts
-% his input interval to centered interval [-r,r], then it calls this function
-% twice and returns hull of results returned by that function.
+%  val(x) = sum i=1...length(itay_coeff) itay_coeff(i)*x^(i-1),
+%       where itay_coeff(i) = (i-1 derivative of p)(c) / (i-1)!
 %
-% r >= 0
+%  Returns [inf(val(ir)), sup(val(ir))], where ir = [0..r]
 %
 %--------------------------------------------------------------------------
 % .Input parameters.
 %
+%  itay_coeff ... interval taylor coefficients 
+%                 itay_coeff(i) = (i-1 derivative of p)(c) / (i-1)!
+%  r          ... radius
+%
 %--------------------------------------------------------------------------
 % .Output parameters.
+%
+%  itfh ... itfh = [inf(val(ir)), sup(val(ir))], where ir = [0..r] 
 %
 %--------------------------------------------------------------------------
 % .Implementation details.
@@ -39,35 +47,42 @@ function res = taylor_form_eval_half_(tay_coeff,r)
 %
 %ENDDOC====================================================================
 
-n = length(tay_coeff);
+n = length(itay_coeff);
 
 oldmod = getround();
 
-%todo inf as built-in function!!
-left = inf(tay_coeff(n));
-right = sup(tay_coeff(n));
+left = inf(itay_coeff(n));
+right = sup(itay_coeff(n));
 
+% [inf(val(ir)), sup(val(ir))]
 for i = n-1:-1:1
 
-	setround(1);
+	% right should be max
 	if (right > 0)
-		right = right*r + sup(tay_coeff(i));
+		setround(1);
+		% r >= 0, sup(itay_coeff(i)) >= inf(itay_coeff(i))
+		right = right*r + sup(itay_coeff(i));
 	else 
-		% choose 0 to maximize
-		right = sup(tay_coeff(i));
+		% choose 0 form [0,r] to maximize sup(right)
+		right = sup(itay_coeff(i));
 	end
 
-	setround(-1);
+	% left should be min
 	if (left < 0)
-		left = left*r + inf(tay_coeff(i));
+		setround(-1);
+		% r >= 0, sup(itay_coeff(i)) >= inf(itay_coeff(i))
+		left = left*r + inf(itay_coeff(i));
 	else
 		% choose 0 to minimize
-		left = inf(tay_coeff(i));
+		left = inf(itay_coeff(i));
 	end
 
+	% [left, right]
+	% = range of itay_coeff(i) + sum j=1..n-i x^j*itay_coeff(i+j)
+	% x is from [0..r] 
 end
 
-res = infsup(left,right);
+itfh = infsup(left,right);
 
 setround(oldmod);
 end
