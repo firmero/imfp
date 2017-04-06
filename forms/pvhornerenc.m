3737d321a06ecb5b487335f349d931a3f80e8f35
Roman <romanfirment@gmail.com>
2017-04-06 23:46:08 +0200

    commented and fixed verification

--- forms/pvhornerenc.m
+++ forms/pvhornerenc.m
@@ -1,4 +1,4 @@
-function [res, certainly_ok] = pvhornerenc(polynomial_coefficients, ix)
+function [ihf ver] = pvhornerenc(p, ix)
 %BEGINDOC==================================================================
 % .Author
 %
@@ -7,29 +7,51 @@ function [res, certainly_ok] = pvhornerenc(polynomial_coefficients, ix)
 %--------------------------------------------------------------------------
 % .Description.
 %
-%
-%  Vector polynomial_coefficients [a_1, a_2, ..., a_n] is interpreted as polynom:
-%
-%	p(x) = a_1*x^(n-1) + a_2*x^(n-2) + ... + a_(n-1)*x^1 + a_n
-%
-%  Return:
-%
-%	res				- Horner form
-%	certainly_ok	- if true then Horner form gives no overestimation 
+%  Compute Horner form of polynomial p over interval ix.
 %
 %--------------------------------------------------------------------------
 % .Input parameters.
 %
-%  ix ... interval x
 %  p  ... vector of polynomial coefficients [a_1 ... a_n]
+%  ix ... interval x
+%         greater value leads to tighter enclosure
 %
 %	p(x) = a_1*x^(n-1) + a_2*x^(n-2) + ... + a_(n-1)*x^1 + a_n
 %
 %--------------------------------------------------------------------------
 % .Output parameters.
 %
+%  ihf ... Horner form
+%  ver ... if 1 then Horner form do not overetimate
+%
 %--------------------------------------------------------------------------
 % .Implementation details.
+%
+%  The implementation tests sufficient condition for non-overestimation
+%  of Horner form. The result of test is returned by variable ver.
+%
+%  The condition is: if the intersection of io and interior of ix
+%  is empty then Horner form is exact. 
+%
+%  io is the hull of all real x such that q_i(x) = 0 for some i in 1..n,
+%  n = length(p). It's evident that 0 is in io, if degree is more than 0.
+%
+%  q_i is defined for i = 1..n:
+%
+%  q_1(x)     = a_1
+%  q_i(x)     = q_(i-1)(x)*x + a_i  for i = 1..n
+%
+%  The range of Horner form of p is obviously q_n(ix).
+%
+%  The emptiness of intersection of io and interior of ix is equivalent
+%  to satisfying the following conditions:
+%
+%  inf(x) >= 0 and inf(HF(q_i,x)) >= 0 for i=1..n
+%    or	
+%  sup(x) <= 0 and inf(sgn_i*HF(q_i,x)) >= 0 for i=1..n
+%    where for j = 1..n
+%      sgn_j =  1 if j is odd
+%      sgn_j = -1 if j is even
 %
 %--------------------------------------------------------------------------
 % .License.
@@ -49,59 +71,50 @@ function [res, certainly_ok] = pvhornerenc(polynomial_coefficients, ix)
 
 ix = intval(ix);
 
-n = length(polynomial_coefficients);
-if (n < 1)
-	res = intval(0);
-	certainly_ok = true;
-	return
-end
-% allocate vector
-p = repmat(intval(0),1,n);
+% allocate vector for polynomial q
+n = length(p);
+iq = repmat(intval(0),1,n);
 
-p(1) = intval(polynomial_coefficients(1));
+iq(1) = p(1);
 
 for i = 2:n
-	p(i) = p(i-1) * ix + polynomial_coefficients(i);
+	iq(i) = iq(i-1)*ix + p(i);
 end
 
-res = p(n);
+ihf = iq(n);
 
-% tests for covering overestimation interval
 if (inf(ix) == sup(ix))
-	certainly_ok = true; % what if coefficients are intervals
+	ver = true;
 	return
 end
 
-if (in(0,intval(ix)))
-	certainly_ok = false; % what if coefficients are intervals
+% testing disjointness of overestimation interval with ix
+% trivial test
+if (in0(0,ix) && n>1 )
+	ver = false;
 	return
 end
 
-
-if (isa(polynomial_coefficients(1),'intval'))
-	certainly_ok = false;
-	return
-end
-
-% !! not working on interval coefficients !!
-sgn = sign(polynomial_coefficients(1));
-
-if (inf(ix) >= 0 )		% on right
-	for i = 2:n-1
-		if (inf(sgn*p(i)) < 0)
-			certainly_ok = false;
+% testing disjointness of overestimation interval with ix
+if (inf(ix) >= 0)
+	% on the right
+	for i = 1:n
+		if (inf(iq(i)) < 0)
+			ver = false;
 			return
 		end
 	end
-else	% on left
-	for i = 2:n-1
+else
+	% on the left
+	sgn = 1;
+	for i = 1:n
+		if (inf(sgn*iq(i)) < 0)
+			ver = false;
+			return
+		end
 		sgn = sgn * (-1);
-		if (inf(sgn*p(i)) < 0)
-			certainly_ok = false;
-			return
-		end
 	end
 end
 
-certainly_ok = true;
+ver = true;
 end
